07e2e7c610a63eecf3f63166b3d8fddb80c8ed69
joshua <joshmanx9@gmail.com>
2025-10-19 08:12:19 -0700

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,8 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.request',
+                'django.template.context_processors.debug',  # Add this
+                'django.template.context_processors.request',  # This should be first
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
